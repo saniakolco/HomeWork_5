3d6626f20dca6e597ee84aa8f79a5fd3f0a38f08
Александр Коваленко <aleksandrkovalenko@iMac-Aleksandr.local>
2022-11-14 19:05:28 +0400

    first commit

---
+++ Homework_5.1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Написать цикл, который принимает на вход два числа (A и B) и
+//возводит число A в натуральную степень B.
+
+System.Console.WriteLine("Введите число A ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число B ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int degree = a;
+
+for (int i = 1; i < b; i++)
+{ 
+    degree = degree * a;
+}
+
+Console.WriteLine("A в степени B = " + degree);
---
+++ Homework_5.3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напиcать программу, которая задаёт массив из N элементов 
+//и выводит их на экран.
+
+System.Console.WriteLine("Установите длинну массива: ");
+int lengthArray = Convert.ToInt32(Console.ReadLine());
+
+int[] randomArray = new int[lengthArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
